6918b9b6db8ae44dfc85b4585b8a93b6751f440e
Prateeksha Ranjan <prateeksha.67@gmail.com>
2021-12-16 17:41:08 +0530

    Updated for Deployment

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/placement_DB");
+mongoose.connect(`mongodb+srv://${process.env.username}:${process.env.password}@cluster0.zygzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+// ("mongodb://localhost/placement_DB");
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Error Connecting to MongoDB"));
